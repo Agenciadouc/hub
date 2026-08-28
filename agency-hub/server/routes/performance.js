75fc2e22a80a8671e0f15a564103d43faed7f718
Joao Soares <joaoluizsoares.m@gmail.com>
2026-08-28 10:16:10 -0300

    fix(perf): Google Ads API v20 (deprecada) -> v22

--- agency-hub/server/routes/performance.js
+++ agency-hub/server/routes/performance.js
@@ -31,7 +31,7 @@ const getKiwifyClientId = () => process.env.KIWIFY_CLIENT_ID
 const getKiwifyClientSecret = () => process.env.KIWIFY_CLIENT_SECRET
 const getKiwifyAccountId = () => process.env.KIWIFY_ACCOUNT_ID
 const META_BASE = 'https://graph.facebook.com/v21.0'
-const GADS_API = 'https://googleads.googleapis.com/v20'
+const GADS_API = 'https://googleads.googleapis.com/v22'
 const GA4_API = 'https://analyticsdata.googleapis.com/v1beta'
 
 // =====================================================================
